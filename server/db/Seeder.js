fbbaac6b5ac98bc45faeed2be7a8eb95af903ce1
yousif Alasfar <yousif.alasfar@gmail.com>
2022-03-22 08:46:43 -0700

    finish schema and seeder

--- server/db/Seeder.js
+++ server/db/Seeder.js
@@ -12,22 +12,54 @@ const __dirname = path.dirname(__filename)
 
 //assemble where the text file is located
 const petTypesPath = path.join(__dirname, "../../pet_types.txt")
-
+const petsPath = path.join(__dirname, "../../pets.txt")
 
 class Seeder {
   static async seed() {
+  
     LineReader.eachLine(petTypesPath, async (line, last, done) => {
       const [name, imgUrl, description] = line.split(";")
       const queryString =
       "INSERT INTO pet_types (name, img_url, description) VALUES ($1, $2, $3) RETURNING id;"
       try {
         const result = await pool.query(queryString, [name, imgUrl, description])
-        
-        if (last) {
-          pool.end()
-        }
         done()
       } catch (error) {
+        console.log(`Error: ${error}`)
+        done()
+      }
+    })
+    
+    LineReader.eachLine(petsPath, async (line, last, done) => {
+      const [
+        name,
+        imgUrl,
+        age,
+        vaccinationStatus,
+        adoptionStory,
+        availableForAdoption,
+        petTypesId
+      ] = line.split(";")
+
+      
+        const queryString =
+          "INSERT INTO pets (name, img_url, age, vaccination_status, adoption_story,available_for_adoption, pet_type_id) VALUES ($1, $2, $3, $4, $5, $6, $7);"
+          try{
+            await pool.query(queryString, [
+              name,
+              imgUrl,
+              age,
+              vaccinationStatus,
+              adoptionStory,
+              availableForAdoption,
+              petTypesId
+            ])
+            if(last){
+              console.log("seeding complete");
+              pool.end()
+            }
+            done()
+          }catch (error) {
         console.log(`Error: ${error}`)
         done()
       }
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,4 +1,4 @@
-  DROP TABLE IF EXISTS  pet_types;
+  DROP TABLE IF EXISTS pet_types, pets, adoption_applications CASCADE;
 
   CREATE TABLE pet_types(
     id SERIAL PRIMARY KEY,
@@ -7,3 +7,23 @@
     description TEXT
   );
 
+  CREATE TABLE pets(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    img_url VARCHAR (255) NOT NULL,
+    age INTEGER, 
+    vaccination_status BOOLEAN NOT NULL DEFAULT false,
+    adoption_story TEXT NOT NULL,
+    available_for_adoption BOOLEAN NOT NULL DEFAULT true,
+    pet_type_id INTEGER REFERENCES pet_types(id)
+  );
+
+  CREATE TABLE adoption_applications(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255) NOT NULL,
+    phone_number VARCHAR (255) NOT NULL,
+    home_status VARCHAR (255) NOT NULL,
+    email VARCHAR (255) NOT NULL,
+    application_status VARCHAR (255) NOT NULL,
+    pet_id INTEGER REFERENCES pets(id)
+  );
